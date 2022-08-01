67e735679338712d70a6af4905a533d7cf9f2018
Bidhan Devkota <76572769+bibeast123@users.noreply.github.com>
2022-08-01 17:23:19 -0500

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Table } from '@jpmorganchase/perspective';
+import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
 
